e13d9f0e0c6f9542bd5d1d66d44261da80a9eba5
sixfwa <farhanaliw6@gmail.com>
2020-06-05 20:21:23 +0100

    server connects to the database

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const database = config.get("mongoURI");
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(database, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connection Established");
+  } catch (error) {
+    console.log(error.message);
+
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+module.exports = connectToDatabase;
