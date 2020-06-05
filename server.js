fe3a31dd04f4707ad520382b87a203da601dbbed
sixfwa <farhanaliw6@gmail.com>
2020-06-05 23:07:43 +0100

    middleware added and user routes

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 // Connect to the database
 connectToDatabase();
 
+// Initialise middleware
+app.use(express.json({ extended: false }));
+
+// Define the routes
+app.use("/api/users", require("./routes/api/users"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
