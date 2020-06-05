dbe2e9b6ba6ec30502386bd2b7eec089452b95c7
sixfwa <farhanaliw6@gmail.com>
2020-06-05 20:21:39 +0100

    connection to the database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectToDatabase = require("./config/db");
 
 const app = express();
+
+// Connect to the database
+connectToDatabase();
 
 const PORT = process.env.PORT || 5000;
 
