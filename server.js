4bc4d5086f26784e2fb905d19a785068779f97a4
sixfwa <farhanaliw6@gmail.com>
2020-06-06 02:23:37 +0100

    calorie route added to server

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/calorie", require("./routes/api/calorie"));
 
 const PORT = process.env.PORT || 5000;
 
