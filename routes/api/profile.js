6f28c768fdfe84945591815c36d57830ad697a50
sixfwa <farhanaliw6@gmail.com>
2020-06-06 01:21:59 +0100

    Create or update a profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const auth = require("../../middleware/auth");
 
@@ -29,3 +30,45 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+/*
+    @route  POST     api/profile
+    @desc   Create or update a user profile
+    @access Private
+*/
+router.post("/", auth, async (req, res) => {
+  const { goal, average } = req.body;
+
+  // Build a profile object
+  const profileFields = {};
+
+  profileFields.user = req.user.id;
+
+  if (goal) profileFields.goal = goal;
+  if (average) profileFields.average = average;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    // If a profile exists then update
+    if (profile) {
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true }
+      );
+
+      return res.json(profile);
+    }
+
+    // Otherwise if a profile does not exist
+    profile = new Profile(profileFields);
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
